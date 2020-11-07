d3a496954ddbd2fb4acc773ded394402a328d2af
HAR Bast no1 <57585542+Rushi136@users.noreply.github.com>
2020-11-07 16:40:03 +0530

    Add files via upload

---
+++ ALAS.py
@@ -0,0 +1,26 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik(' all YouTube love MR.RUSHI............................')
+mengetik('.')
+mengetik('.')
+mengetik(' I miss you like share comment subscribe my YouTube channel.........')
+mengetik('.. creta Mr Rushi sahu......................')
+mengetik('.BHATAPALA...... HELLO')
+mengetik('.ALL HACKING TIRECK')
+mengetik('......................................')
+mengetik('...ILOVE YOU (R) ')
+mengetik('...')
+mengetik('...')
+mengetik('RUSHI+LILI!')
+mengetik('100%')
+mengetik('Odisha Kalahandi Kala Munda
+mengetik('my YouTube channel
+mengetik('............Rushi sahu')
+mengetik('All YouTube iLOve you')
+mengetik('RUSHI')
